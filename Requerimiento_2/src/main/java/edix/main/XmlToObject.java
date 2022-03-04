396bea46cf35459e6584beb2b09f98f513209e53
Guiller <iphoneguiller91@gmail.com>
2022-03-04 11:49:22 +0100

    agregada funcionalidad
    Funcion de listar el xml

--- Requerimiento_2/src/main/java/edix/main/XmlToObject.java
+++ Requerimiento_2/src/main/java/edix/main/XmlToObject.java
@@ -1,12 +1,15 @@
 package edix.main;
 
 import java.io.File;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import edix.beans.Cancion;
+import edix.beans.Concierto;
 import edix.beans.Festival;
+import edix.beans.Grupo;
+import edix.beans.Integrante;
 
 public class XmlToObject {
 
@@ -19,7 +22,7 @@ public class XmlToObject {
 			File fichero = new File("festival.xml");
 			if (fichero.exists()) {
 				Festival festival = (Festival) u.unmarshal(fichero);			
-				System.out.println(festival);
+				listar(festival);
 			} else {
 				System.out.println("Fichero XML festival.xml no encontrado");
 			}
@@ -28,6 +31,33 @@ public class XmlToObject {
 			System.out.println(e.getMessage());
 		}
 
+	}
+	
+	static void listar(Festival festival) {
+		
+		System.out.println("El " + festival.getNombre() + " dara comienzo el " + festival.getFecha() + " " + festival.getHora());
+		System.out.println("En la calle " + festival.getDireccion().getCalle() + ", " + festival.getDireccion().getNumero()
+							+", " + festival.getDireccion().getCiudad());
+		System.out.println("Los conciertos son los siguientes: ");
+		
+		for (int i=0; i<festival.getConciertos().size();i++) {
+			Concierto c = festival.getConciertos().get(i);
+			Grupo grupo = c.getGrupo();
+			System.out.println("--------------------------------\n");
+			System.out.println(c.getNombre() + " empieza a las " + c.getHora());
+			System.out.println("Tocara : " + grupo.getNombre());
+			System.out.println("\n<---INTEGRANTES--->\n");
+			for(Integrante integrante : grupo.getIntegrantes()) {
+				System.out.println(integrante.getNombre() + ", rol: " + integrante.getRol() + ", edad: " + integrante.getEdad());
+			}
+			System.out.println("\n<---CANCIONES--->\n");
+			for(Cancion cancion : grupo.getCanciones()) {
+				System.out.println(cancion.getNombre() + ", duracion: " + cancion.getDuracion() );
+			}
+			
+			
+		}
+		
 	}
 
 }
